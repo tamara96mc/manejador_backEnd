0c69e8114aac313b98c6c540ba8d1b9e8144f3bc
tamara96mc <tamara.96mc@gmail.com>
2022-01-09 12:26:11 +0100

    cambio el el controlador de dato getBytelefono

--- controllers/datoController.js
+++ controllers/datoController.js
@@ -76,7 +76,7 @@ datoController.update = (req, res) => {
 //GET dato by Title from database 
 //FindByTitle
   datoController.getByTelefono = (req, res) => {
-    sequelize_conexion.query(`SELECT campos.nombre, datos.valor FROM datos
+    sequelize_conexion.query(`SELECT campos.nombre, datos.valor, datos.id FROM datos
     INNER JOIN Campos ON datos.campoId=campos.id WHERE telefono
     ='${req.params.telefono}' `, { model: campoModel })
       .then(data => {
